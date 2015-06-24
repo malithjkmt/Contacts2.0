210344a1d6e10b643385b1535727f5dd0f79abe0
Malith <malith.13@cse.mrt.ac.lk>
2015-06-25 05:42:28 +1100

    connect db, create Person class

---
+++ Contacts2.0/src/app/Person.java
@@ -0,0 +1,270 @@
+
+package app;
+
+/**
+ *
+ * @author Malith
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String group;
+    private String tags;
+    private String nic;
+    private String sex;
+    private String mobileOne;
+    private String mobileTwo;
+    private String home;
+    private String office;
+    private String fax;
+    private String personalAddress;
+    private String officeAddress;
+    private String business;
+    private String notes;
+    private String birthday;
+   // private Blob picture; // later
+    private String acNumber;
+    private String nickName;
+    private String branch;
+    private String cifNo;
+    private String acType;
+    private String emailPersonal;
+    private String emailBusiness;
+    private String webPagePersonal;
+    private String webPageBusiness;
+
+    //constructure (without pic)
+    public Person(String firstName, String lastName, String group, String tags, String nic, String sex, String mobileOne, String mobileTwo, String home, String office, String fax, String personalAddress, String officeAddress, String business, String notes, String birthday, String acNumber, String nickName, String branch, String cifNo, String acType, String emailPersonal, String emailBusiness, String webPagePersonal, String webPageBusiness) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.tags = tags;
+        this.nic = nic;
+        this.sex = sex;
+        this.mobileOne = mobileOne;
+        this.mobileTwo = mobileTwo;
+        this.home = home;
+        this.office = office;
+        this.fax = fax;
+        this.personalAddress = personalAddress;
+        this.officeAddress = officeAddress;
+        this.business = business;
+        this.notes = notes;
+        this.birthday = birthday;
+        this.acNumber = acNumber;
+        this.nickName = nickName;
+        this.branch = branch;
+        this.cifNo = cifNo;
+        this.acType = acType;
+        this.emailPersonal = emailPersonal;
+        this.emailBusiness = emailBusiness;
+        this.webPagePersonal = webPagePersonal;
+        this.webPageBusiness = webPageBusiness;
+    }
+    
+
+    //setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setMobileOne(String mobileOne) {
+        this.mobileOne = mobileOne;
+    }
+
+    public void setMobileTwo(String mobileTwo) {
+        this.mobileTwo = mobileTwo;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public void setPersonalAddress(String personalAddress) {
+        this.personalAddress = personalAddress;
+    }
+
+    public void setOfficeAddress(String officeAddress) {
+        this.officeAddress = officeAddress;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAcNumber(String acNumber) {
+        this.acNumber = acNumber;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public void setCifNo(String cifNo) {
+        this.cifNo = cifNo;
+    }
+
+    public void setAcType(String acType) {
+        this.acType = acType;
+    }
+
+    public void setEmailPersonal(String emailPersonal) {
+        this.emailPersonal = emailPersonal;
+    }
+
+    public void setEmailBusiness(String emailBusiness) {
+        this.emailBusiness = emailBusiness;
+    }
+
+    public void setWebPagePersonal(String webPagePersonal) {
+        this.webPagePersonal = webPagePersonal;
+    }
+
+    public void setWebPageBusiness(String webPageBusiness) {
+        this.webPageBusiness = webPageBusiness;
+    }
+
+    // getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public String getMobileOne() {
+        return mobileOne;
+    }
+
+    public String getMobileTwo() {
+        return mobileTwo;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getPersonalAddress() {
+        return personalAddress;
+    }
+
+    public String getOfficeAddress() {
+        return officeAddress;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getAcNumber() {
+        return acNumber;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public String getCifNo() {
+        return cifNo;
+    }
+
+    public String getAcType() {
+        return acType;
+    }
+
+    public String getEmailPersonal() {
+        return emailPersonal;
+    }
+
+    public String getEmailBusiness() {
+        return emailBusiness;
+    }
+
+    public String getWebPagePersonal() {
+        return webPagePersonal;
+    }
+
+    public String getWebPageBusiness() {
+        return webPageBusiness;
+    }
+    
+    
+    
+}
